45b7b683d7da502a4415f1ace6a0638842b93f27
Himanshu-Narayan <narayanhimanshu855@gmail.com>
2026-08-12 21:49:39 +0530

    Time: 6 ms (84.15%) | Memory: 46.3 MB (80.87%) - LeetSync

--- 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.java
+++ 1632-number-of-good-ways-to-split-a-string/number-of-good-ways-to-split-a-string.java
@@ -6,7 +6,6 @@ class Solution {
         int leftDistinct = 0;
         int rightDistinct = 0;
 
-        // Initially, everything is on the right
         for (char c : s.toCharArray()) {
             if (rightFreq[c - 'a'] == 0) {
                 rightDistinct++;
@@ -19,7 +18,6 @@ class Solution {
         for (int i = 0; i < s.length() - 1; i++) {
             char c = s.charAt(i);
 
-            // Move character from right -> left
             if (leftFreq[c - 'a'] == 0) {
                 leftDistinct++;
             }
