f9402e8651a32c5ed494a32c7a151baed798b089
Himanshu-Narayan <narayanhimanshu855@gmail.com>
2026-08-17 10:20:29 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (40.21%) - LeetSync

---
+++ 4305-check-adjacent-digit-differences/check-adjacent-digit-differences.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isAdjacentDiffAtMostTwo(String s) {
+        
+  
+    for(int i =0;i<s.length()-1;i++){
+        int a = s.charAt(i) -'0';
+        int b = s.charAt(i+1) - '0';
+        if(Math.abs(b-a)>=3){
+            return false;
+        }
+    }
+      
+return true;
+
+
+    }
+}
