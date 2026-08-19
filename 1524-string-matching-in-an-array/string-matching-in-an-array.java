0e2e47118c4470974fef983d5000d4b32bc66f88
Himanshu-Narayan <narayanhimanshu855@gmail.com>
2026-08-19 11:00:21 +0530

    Time: 12 ms (5.38%) | Memory: 43.4 MB (90.76%) - LeetSync

--- 1524-string-matching-in-an-array/string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i =0;i<words.length;i++){
             String current = words[i];
             for(int j =0;j<words.length;j++){
-                if(!res.contains(words[j]) && current.indexOf(words[j])!=-1 && !current.equals(words[j])) {
+                if(!res.contains(words[j]) && current.contains(words[j]) && i!=j) {
                     res.add(words[j]);
                 }
             }
