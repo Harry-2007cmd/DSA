588592da84fcdc07cf577edd981a0925a9e5affd
Himanshu-Narayan <narayanhimanshu855@gmail.com>
2026-08-23 10:14:56 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (56.67%) - LeetSync

---
+++ 4378-nearest-available-drone/nearest-available-drone.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int nearestDrone(int[][] drones, int[] target) {
+        
+        int ans = Integer.MAX_VALUE;
+        int res =-1;
+        
+        for(int i =0;i<drones.length;i++){
+            int dist = Math.abs(drones[i][0] - target[0]) + Math.abs(drones[i][1] - target[1]);
+            if(dist <= drones[i][2]){
+               if(dist<ans){
+               ans = dist;
+               res= i;}
+            } 
+        }
+return res;
+    }
+}
